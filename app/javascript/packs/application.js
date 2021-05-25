68f0e49454dffe19fd5351a0f43898a22b21a353
claire gautier <claire.gautier@edhec.com>
2021-05-25 11:21:27 +0200

    test lecture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { initMapbox } from '../components/init_mapbox';
+import { initAutocomplete } from '../components/init_autocomplete';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+  initAutocomplete();
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
